5077a903263589ab0494ef4aea66ea39d02e4d38
llj <2361915181@qq.com>
2022-10-30 07:58:59 +0000

    v11.1: 增加了 release 版本的编译脚本.

--- build_release.sh
+++ build_release.sh
@@ -1,1 +1,1 @@
-mkdir -p build && rm -rf build/miniob/ && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ && make -j4
+mkdir -p build && rm -rf build/miniob/ && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-fsanitize=address -g" -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ && make -j4
